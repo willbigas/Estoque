ce8ba18c01956984b71b13cd9a981c0b1f58b8b6
willbigas <williambmauro@hotmail.com>
2019-01-18 09:02:50 -0200

    tudo

--- src/br/com/ecall/ECall.java
+++ src/br/com/ecall/ECall.java
@@ -2,12 +2,15 @@ package br.com.ecall;
 
 import br.com.ecall.view.PainelLogin;
 import br.com.ecall.view.PainelPrincipal;
+import br.com.ecall.view.PainelTelefone;
+import br.com.ecall.view.PainelUsuario;
+import br.com.ecall.view.PainelUsuarioEdicao;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 
 public class Ecall {
-
-    public static void main(String[] args) throws Exception{
+    
+    public static void main(String[] args) throws Exception {
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         JanelaLogin();
         
@@ -22,13 +25,41 @@ public class Ecall {
         painelLogin.setVisible(true);
     }
     
-     public static void JanelaPrincipal() {
+    public static void JanelaPrincipal() {
         PainelPrincipal painelPrincipal = new PainelPrincipal();
-        painelPrincipal.setTitle("E-CALL - Gerenciador de Atendimentos");
-        painelPrincipal.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        painelPrincipal.setTitle("E-CALL - GERENCIADOR DE ATENDIMENTOS");
+        painelPrincipal.setSize(800, 600);
         painelPrincipal.setLocationRelativeTo(null);
         painelPrincipal.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         painelPrincipal.setVisible(true);
     }
-
+    
+    public static void JanelaUsuario() {
+        PainelUsuario painelUsuario = new PainelUsuario();
+        painelUsuario.setTitle("E-CALL - USUARIO");
+        painelUsuario.setSize(800, 600);
+        painelUsuario.setLocationRelativeTo(null);
+        painelUsuario.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        painelUsuario.setVisible(true);
+    }
+    
+    public static void JanelaUsuarioEdicao() {
+        PainelUsuarioEdicao painelUsuarioEdicao = new PainelUsuarioEdicao();
+        painelUsuarioEdicao.setTitle("E-CALL - EDICAO DE USUARIO");
+        painelUsuarioEdicao.setSize(546, 500);
+        painelUsuarioEdicao.setLocationRelativeTo(null);
+        painelUsuarioEdicao.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        painelUsuarioEdicao.setVisible(true);
+    }
+    
+    
+    public static void JanelaTelefone() {
+        PainelTelefone painelTelefone = new PainelTelefone();
+        painelTelefone.setTitle("E-CALL - TELEFONES DE CONTATO");
+        painelTelefone.setSize(800, 600);
+        painelTelefone.setLocationRelativeTo(null);
+        painelTelefone.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        painelTelefone.setVisible(true);
+    }
+    
 }
