6872554798b2a1c548c89ed2b5a400ba67b537f0
willbigas <williambmauro@hotmail.com>
2019-02-07 17:22:49 -0200

    SQL do banco atualizado.

--- sql.sql
+++ sql.sql
@@ -4,6 +4,13 @@ use bwstock;
 
 /** Criando Tabelas **/
 
+CREATE TABLE categoriaProduto(
+id INT(11) not null auto_increment,
+nome varchar(25),
+descricao varchar(150),
+atualizado timestamp NOT NULL,
+PRIMARY KEY (id));
+
 CREATE TABLE estoqueMovimento(
 id INT(11) not null auto_increment,
 dataEntrada date,
@@ -20,10 +27,12 @@ ean13 INT(13),
 qtdEstoque INT(13),
 precoUnitario decimal(10,2),
 id_estoqueMovimento int,
+id_categoriaProduto int,
 dataCadastro Date,
 ativo boolean,
 atualizado timestamp NOT NULL,
 foreign key(id_estoqueMovimento) references estoqueMovimento(id),
+foreign key(id_categoriaProduto) references categoriaProduto(id),
 PRIMARY KEY (id));
 
 select * from estoqueMovimento;
